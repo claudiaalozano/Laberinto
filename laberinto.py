67224dbd3081b795a17c4d9ea256a2eb374a5175
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-08 18:11:26 +0100

    commit 2

--- laberinto.py
+++ laberinto.py
@@ -6,3 +6,14 @@ laberinto = {
     ["X" , "X" , "X" , "X" , "X"]
 
 }
+#posiciones para el inicio de la partida
+x=0
+y=0
+
+print("Comienza el juego. Para finalizarlo debe completar el laberito.")
+print("Usted comienza en la casilla (0,0)")
+
+while (laberinto[y])[x] == "recorrido":
+    dirección_recorrido = input("¿Hacia que dirreción desea moverse?:")
+    if dirección_recorrido = "arriba" , "abajo" , "derecha" or "izquierda":
+         
