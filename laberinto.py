1f0b1b3bdeabc6089525245de7a1585ac3a543f1
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-08 16:43:11 +0100

    Commit 1

--- laberinto.py
+++ laberinto.py
@@ -1,8 +1,8 @@
 laberinto = {
-    ["X" , "X" , "X" , "X"],
-    ["abajo" , "X" , "X" , "X"],
-    ["abajo" , "X" , "X" , "X"],
-    ["abajo" , "X" , "X" , "X"],
-    ["abajo" , "X" , "X" , "X"],
-    
+    ["recorrido" , "X" , "X" , "X" , "X"],
+    ["recorrido" , "X" , "recorrido" , "recorrido" , "recorrido"],
+    ["recorrido" , "X" , "recorrido" , "X" , "X"],
+    ["recorrido" , "recorrido" , "recorrido" , "X" , "X"],
+    ["X" , "X" , "X" , "X" , "X"]
+
 }
